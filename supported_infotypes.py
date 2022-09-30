c5918baec200a1383fdc89230deb4fc00464f1b5
Piyush Chauhan <piyush.chauhan@gslab.com>
2022-09-30 15:35:44 +0530

    changed Credit_Card_Number to Credit_Debit_Card_Number

--- supported_infotypes.py
+++ supported_infotypes.py
@@ -1,3 +1,2 @@
-# infotypes_to_use = ['Street_Address', 'Gender', 'Credit_Debit_Card_Number', 'Email_Address', 'Phone_Number', 'Full_Name',
-#                     'Age']
-infotypes_to_use = ['Full_Name']
+infotypes_to_use = ['Street_Address', 'Gender', 'Credit_Debit_Card_Number', 'Email_Address', 'Phone_Number', 'Full_Name',
+                    'Age']
