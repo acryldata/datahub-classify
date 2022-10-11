25a861075c3aa74cb2c6f797d30b812da1cf6a91
Piyush Chauhan <piyush.chauhan@gslab.com>
2022-10-11 17:06:49 +0530

    removed the print statements

--- src/datahub_classify/infotype_predictor.py
+++ src/datahub_classify/infotype_predictor.py
@@ -24,16 +24,11 @@ def get_infotype_function_mapping():
 def predict_infotypes(column_infos, confidence_level_threshold, global_config):
     # assert type(column_infos) == list, "type of column_infos should be list"
     infotype_function_map = get_infotype_function_mapping()
-    print(f"Total columns to be processed --> {len(column_infos)}")
-    print(f"Confidence Level Threshold set to --> {confidence_level_threshold}")
-    print("===========================================================")
-    logger.info(f"Time --->  {datetime.now()}", )
     logger.info(f"Total columns to be processed --> {len(column_infos)}")
     logger.info(f"Confidence Level Threshold set to --> {confidence_level_threshold}")
     logger.info("===========================================================")
     for column_info in column_infos:
         logger.debug(f"processing column: {column_info.metadata.name}")
-        print(f"processing column: {column_info.metadata.name}")
         # iterate over all infotype functions
         proposal_list = []
         for infotype, infotype_fn in infotype_function_map.items():
@@ -53,7 +48,7 @@ def predict_infotypes(column_infos, confidence_level_threshold, global_config):
                           (infotype, column_info.metadata.name))
             except Exception as e:
                 # traceback.print_exc()
-                logger.warning(f"Failed to extract info type due to {e}", exc_info=e)
+                logger.warning(f"Failed to extract info type due to {e}")
         column_info.infotype_proposals = proposal_list
     logger.info("=============RUN COMPLETE ==============================")
 
