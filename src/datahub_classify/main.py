166b3dbca4520344bff4540a06f8f449363daf0d
Piyush Chauhan <piyush.chauhan@gslab.com>
2022-10-11 17:09:58 +0530

    removed datasets not having license for commercial use

--- src/datahub_classify/main.py
+++ src/datahub_classify/main.py
@@ -12,7 +12,6 @@ current_dir = os.getcwd()
 logging_directory = current_dir + "/logs/logs.log"
 
 def get_public_data(input_data_path):
-    # TODO: Iterate over the directory and load all datasets
     data1 = pd.read_csv(input_data_path + "UCI_Credit_Card.csv")
     data2 = pd.read_csv(input_data_path + "Age2_address1_credit_card3.csv")
     data3 = pd.read_csv(input_data_path + "list_of_real_usa_addresses.csv")
@@ -20,39 +19,34 @@ def get_public_data(input_data_path):
     data5 = pd.read_csv(input_data_path + "Credit_Card2.csv")
     data6 = pd.read_csv(input_data_path + "catalog.csv")
     data7 = pd.read_csv(input_data_path + "iban.csv")
-    data8 = pd.read_csv(input_data_path + "USA_cars_datasets.csv")
-    data9 = pd.read_csv(input_data_path + "email_1.csv")
-    data10 = pd.read_csv(input_data_path + "email_2.csv")
-    data11 = pd.read_csv(input_data_path + "email_3.csv")
     data12 = pd.read_csv(input_data_path + "2018-seattle-business-districts.csv")
     data13 = pd.read_csv(input_data_path + "Customer Segmentation.csv")
     data14 = pd.read_csv(input_data_path + "application_record.csv")
     data15 = pd.read_csv(input_data_path + "Airbnb_Open_Data.csv")
     data16 = pd.read_csv(input_data_path + "Book1.xlsx-credit-card-number.csv")
     data17 = pd.read_csv(input_data_path + "Aliases.csv")
-    data18 = pd.read_csv(input_data_path + "athletes.csv")
-    data19 = pd.read_csv(input_data_path + "coaches.csv")
-    data20 = pd.read_csv(input_data_path + "curling_results.csv")
     data21 = pd.read_csv(input_data_path + "Emails.csv")
-    data22 = pd.read_csv(input_data_path + "hockey_players_stats.csv")
-    data23 = pd.read_csv(input_data_path + "hockey_results.csv")
-    data24 = pd.read_csv(input_data_path + "medals.csv")
     data25 = pd.read_csv(input_data_path + "Persons.csv")
-    data26 = pd.read_csv(input_data_path + "technical_officials.csv")
-    return [data1, data2, data3, data4, data5, data6, data7, data8, data9,
-            data10, data11, data12, data13, data14, data15, data16, data17,
-            data18, data19, data20, data21, data22, data23, data24, data25, data26]
+    data27 = pd.read_csv(input_data_path + "Bachelor_Degree_Majors.csv")
+    data28 = pd.read_csv(input_data_path + "CrabAgePrediction.csv")
+    data29 = pd.read_csv(input_data_path + "Salary_Data.csv")
+    data30 = pd.read_csv(input_data_path + "drug-use-by-age.csv")
+    return {'data1': data1, 'data2': data2, 'data3': data3, 'data4': data4, 'data5': data5,
+            'data6': data6, 'data7': data7, 'data12': data12, 'data13': data13, 'data14': data14,
+            'data15': data15,'data16': data16, 'data17': data17, 'data21': data21, 'data25': data25,
+            'data27': data27, 'data28': data28, 'data29': data29, 'data30': data30}
 
 
-def populate_column_info_list(data_list):
+def populate_column_info_list(public_data_list):
     column_info_list = []
     actual_labels = []
-    for i, data in enumerate(data_list):
+    for i, (dataset_name, data) in enumerate(public_data_list.items()):
         for col in data.columns:
             fields = {
                 'Name': col,
                 'Description': f'This column contains name of the {col}',
-                'Datatype': 'str'
+                'Datatype': 'str',
+                'Dataset_Name': dataset_name
             }
             metadata = Metadata(fields)
             if len(data[col].dropna()) > 1000:
@@ -62,7 +56,6 @@ def populate_column_info_list(data_list):
             col_info = ColumnInfo(metadata, values)
             column_info_list.append(col_info)
             actual_labels.append(col)
-    # return column_info_list, actual_labels
     return column_info_list
 
 
@@ -92,13 +85,13 @@ def run_test(input_data_path):
 
 
 if __name__ == '__main__':
-    # import logging.config
-    logging.basicConfig(filename=logging_directory, filemode='a',format='%(asctime)s %(levelname)-8s %(message)s',
+    logging.basicConfig(format='%(asctime)s %(levelname)-8s %(message)s',
                         encoding='utf-8', datefmt='%Y-%m-%d %H:%M:%S', level=logging.INFO)
     logger.info("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
     logger.info(f"--------------------STARTING RUN--------------------  ")
     logger.info("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
-    # input_data_dir = "C:\\Glossary_Terms\\datasets\\"
+    logger.info(f"Start Time --->  {datetime.now()}", )
+    # input_data_dir = "C:\\Glossary_Terms_Git\\datahub-classify\\test\\datasets\\"
     # input_data_dir = '../../../../../../jupyter/office_project/acryl_glossary_term/dataset/'
     input_data_dir = './'
     run_test(input_data_dir)
