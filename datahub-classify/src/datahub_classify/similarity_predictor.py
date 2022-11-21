f29456e01448f5cb709de84fcdd39480244d4e9b
Piyush Chauhan <piyush.chauhan@gslab.com>
2022-11-21 17:35:15 +0530

    removed verbose print statements

--- datahub-classify/src/datahub_classify/similarity_predictor.py
+++ datahub-classify/src/datahub_classify/similarity_predictor.py
@@ -13,7 +13,7 @@ def check_similarity(table_info1: TableInfo, table_info2: TableInfo):
     print("total pairs --> ", len(table_info1.column_infos)* len(table_info2.column_infos))
     for col_info1 in table_info1.column_infos:
         for col_info2 in table_info2.column_infos:
-            print("processing pair: ", (col_info1.metadata.name, col_info2.metadata.name))
+            # print("processing pair: ", (col_info1.metadata.name, col_info2.metadata.name))
             overall_column_similarity_score = compute_column_similarity(col_info1, col_info2, overall_table_similarity_score)
             column1_id = col_info1.metadata.column_id
             column2_id = col_info2.metadata.column_id
