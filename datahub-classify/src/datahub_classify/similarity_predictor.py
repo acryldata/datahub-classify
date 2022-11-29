c2301d97f315c1dff1b394393daad7d860cb8620
Piyush Chauhan <piyush.chauhan@gslab.com>
2022-11-29 14:14:10 +0530

    Moved read_glov_vector function call inside check_similarity function

--- datahub-classify/src/datahub_classify/similarity_predictor.py
+++ datahub-classify/src/datahub_classify/similarity_predictor.py
@@ -7,11 +7,15 @@ import os
 logger = logging.getLogger(__name__)
 current_wdr = os.path.dirname(os.path.abspath(__file__))
 glove_vec = os.path.join(current_wdr, "glove.6B.50d.txt")
-word_to_vec_map = read_glove_vector(glove_vec)
+word_to_vec_map = None
 
 
-def check_similarity(table_info1: TableInfo, table_info2: TableInfo,  word_to_vec_map: str = word_to_vec_map):
+def check_similarity(table_info1: TableInfo, table_info2: TableInfo):
     logger.info(f"Finding table similarity between Table {table_info1.metadata.table_id} and {table_info2.metadata.table_id}")
+    global word_to_vec_map
+    if not word_to_vec_map:
+        logger.debug("Loading Glove Embeddings")
+        word_to_vec_map = read_glove_vector(glove_vec)
     overall_table_similarity_score = None
     try:
         overall_table_similarity_score = compute_table_similarity(table_info1, table_info2, word_to_vec_map)
@@ -22,8 +26,6 @@ def check_similarity(table_info1: TableInfo, table_info2: TableInfo,  word_to_ve
     logger.info("====================================")
     logger.info("******************** Finding column similarities **********************")
     logger.info(f"Total pairs --> {len(table_info1.column_infos)* len(table_info2.column_infos)}")
-    # TODO: if we find a column pair with high similarity score, can we skip the pair in further processing
-    # TODO: hypothesis is if table1.colA matches with table2.colB then we will not have any other column in table2 matching with table1.colA
     for col_info1 in table_info1.column_infos:
         for col_info2 in table_info2.column_infos:
             logger.debug(f"Processing pair: {(col_info1.metadata.name, col_info2.metadata.name)}")
