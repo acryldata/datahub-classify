b4e9f918fed22b73db37e47dce48b8cedbfd3e7f
Piyush Chauhan <piyush.chauhan@gslab.com>
2022-11-30 12:05:10 +0530

    Changed the default declarations of column values, parent columns, parent tables to list to avoid mypy errors

--- datahub-classify/src/datahub_classify/helper_classes.py
+++ datahub-classify/src/datahub_classify/helper_classes.py
@@ -23,14 +23,14 @@ class ColumnMetadata:
         self.description = self.meta_info.get("Description", None)
         self.datatype = self.meta_info.get("Datatype", None)
         self.dataset_name = self.meta_info.get("Dataset_Name", None)
-        self.column_id = self.meta_info.get('Column_Id', None)
+        self.column_id = self.meta_info.get("Column_Id", None)
 
 
 @dataclass
 class ColumnInfo:
     metadata: ColumnMetadata
-    parent_columns: Optional[List] = None
-    values: Optional[List] = None
+    parent_columns: List = field(default_factory=list)
+    values: List = field(default_factory=list)
     infotype_proposals: Optional[List[InfotypeProposal]] = None
 
 
@@ -43,14 +43,14 @@ class TableMetadata:
     table_id: str = field(init=False)
 
     def __post_init__(self):
-        self.name = self.meta_info.get('Name', None)
-        self.description = self.meta_info.get('Description', None)
-        self.platform = self.meta_info.get('Platform', None)
-        self.table_id = self.meta_info.get('Table_Id', None)
+        self.name = self.meta_info.get("Name", None)
+        self.description = self.meta_info.get("Description", None)
+        self.platform = self.meta_info.get("Platform", None)
+        self.table_id = self.meta_info.get("Table_Id", None)
 
 
-@dataclass      
+@dataclass
 class TableInfo:
     metadata: TableMetadata
-    parent_tables: list
-    column_infos: list
+    column_infos: List
+    parent_tables: List = field(default_factory=list)
