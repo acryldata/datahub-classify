3bc6f02e41696ae10b1adf3d92c159f55d1989df
Piyush Chauhan <piyush.chauhan@gslab.com>
2022-12-20 17:13:09 +0530

    reduced the verbosity of logger messages (some logs moved to debug level)

--- datahub-classify/src/datahub_classify/infotype_predictor.py
+++ datahub-classify/src/datahub_classify/infotype_predictor.py
@@ -35,9 +35,11 @@ def predict_infotypes(
     infotypes: Optional[List[str]] = None,
 ) -> List[ColumnInfo]:
     infotype_function_map = get_infotype_function_mapping(infotypes, global_config)
-    logger.info(f"Total columns to be processed --> {len(column_infos)}")
-    logger.info(f"Confidence Level Threshold set to --> {confidence_level_threshold}")
-    logger.info("===========================================================")
+    logger.debug(f"Total columns to be processed --> {len(column_infos)}")
+    logger.debug(f"Confidence Level Threshold set to --> {confidence_level_threshold}")
+    logger.debug("===========================================================")
+    basic_checks_failed_columns = []
+    num_cols_with_infotype_assigned = 0
     for column_info in column_infos:
         logger.debug(
             f"processing column: {column_info.metadata.name} -- dataset: {column_info.metadata.dataset_name}"
@@ -45,6 +47,7 @@ def predict_infotypes(
         # iterate over all infotype functions
         proposal_list = []
         for infotype, infotype_fn in infotype_function_map.items():
+
             # get the configuration
             config_dict = global_config[infotype]
 
@@ -67,13 +70,26 @@ def predict_infotypes(
                         )
                         proposal_list.append(infotype_proposal)
                 else:
-                    raise Exception(
-                        f"Failed basic checks for infotype - {infotype}, column - {column_info.metadata.name} and dataset - {column_info.metadata.dataset_name}"
+                    basic_checks_failed_columns.append(
+                        (column_info.metadata.name, column_info.metadata.dataset_name)
                     )
+
             except Exception as e:
                 # traceback.print_exc()
                 logger.warning(f"Failed to extract info type due to {e}")
+        if len(proposal_list) > 0:
+            num_cols_with_infotype_assigned += 1
         column_info.infotype_proposals = proposal_list
-    logger.info("=============RUN COMPLETE ==============================")
+    if len(basic_checks_failed_columns) > 0:
+        basic_checks_failed_columns_set = set(basic_checks_failed_columns)
+        logger.warning(
+            f"Infotype not extracted due to basic checks failure for {len(basic_checks_failed_columns_set)} out of {len(column_infos)} columns.Check DEBUG logs for details"
+        )
+        logger.debug(
+            f"Basic Checks failed for following (column_name, table_name) --> {basic_checks_failed_columns_set}"
+        )
+    logger.debug(
+        f"Infotype assigned to {num_cols_with_infotype_assigned} out of {len(column_infos)} columns"
+    )
 
     return column_infos
