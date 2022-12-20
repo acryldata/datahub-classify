ac523d56395f0a8c2388e3a6c1a00cb32ba6b3c0
Piyush Chauhan <piyush.chauhan@gslab.com>
2022-12-20 17:03:48 +0530

    add Final qualifier to prevent mypy type checking errors

--- datahub-classify/src/datahub_classify/constants.py
+++ datahub-classify/src/datahub_classify/constants.py
@@ -1,10 +1,12 @@
+from typing_extensions import Final
+
 # Input config dictionary keys
 PREDICTION_FACTORS_AND_WEIGHTS = "Prediction_Factors_and_Weights"
 NAME = "Name"
-DESCRIPTION = "Description"
-DATATYPE = "Datatype"
-VALUES = "Values"
-REGEX = "regex"
+DESCRIPTION: Final = "Description"
+DATATYPE: Final = "Datatype"
+VALUES: Final = "Values"
+REGEX: Final = "regex"
 TYPE = "type"
 PREDICTION_TYPE = "prediction_type"
 LIBRARY = "library"
